df83800f5ae32aed4dfd2b1d3aa61c20258ff409
Doğacan Serin <dogacanserin@gmail.com>
2022-10-11 23:49:45 +0300

    .

--- client/src/index.js
+++ client/src/index.js
@@ -13,33 +13,27 @@ import { BasketProvider } from "./contexts/BasketContext";
 
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 const queryClient = new QueryClient({
-	defaultOptions: {
-		queries: {
-			refetchOnMount: false,
-			refetchOnWindowFocus: false,
-		},
-	},
+  defaultOptions: {
+    queries: {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+    },
+  },
 });
 
 ReactDOM.render(
-	<QueryClientProvider client={queryClient}>
-		<ChakraProvider>
-			<AuthProvider>
-				<BasketProvider>
-					<App />
-				</BasketProvider>
-			</AuthProvider>
-		</ChakraProvider>
+  <QueryClientProvider client={queryClient}>
+    <ChakraProvider>
+      <AuthProvider>
+        <BasketProvider>
+          <App />
+        </BasketProvider>
+      </AuthProvider>
+    </ChakraProvider>
 
-		<ReactQueryDevtools initialIsOpen={false} />
-	</QueryClientProvider>,
-	document.getElementById("root")
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
